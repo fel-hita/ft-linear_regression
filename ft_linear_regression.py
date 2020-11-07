3385a7c52bd707f9623158b28f84a464e5fe835f
fel-hita <farouk.elhitary@gmail.com>
2020-11-07 10:18:42 +0100

    parsing works but gradient multiplication overflows

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -1,8 +1,10 @@
 import numpy as np
+from numpy import genfromtxt
 import matplotlib.pyplot as plt
-#windows
-import matplotlib
-matplotlib.use('Agg')
+import csv
+# windows
+# import matplotlib
+# matplotlib.use('Agg')
 
 class GradientDescentLinearRegression:
     def __init__(self, learning_rate=0.01, iterations=1000):
@@ -10,7 +12,7 @@ class GradientDescentLinearRegression:
     
     def fit(self, X, y):
         b = 0
-        m = 5
+        m = 0
         n = X.shape[0]
         for _ in range(self.iterations):
             b_gradient = -2 * np.sum(y - m*X + b) / n
@@ -22,18 +24,32 @@ class GradientDescentLinearRegression:
     def predict(self, X):
         return self.m*X + self.b
 
-np.random.seed(42)
-X = np.array(sorted(list(range(5))*20)) + np.random.normal(size=100, scale=0.5)
-y = np.array(sorted(list(range(5))*20)) + np.random.normal(size=100, scale=0.25)
-print (X)
+if __name__ == '__main__':
+    
+    data = genfromtxt('data.csv',delimiter=',')
+    data = np.delete(data, 0, 0)
+    #km
+    X = data[:,1]
+    #price
+    y = data[:,0]
 
-clf = GradientDescentLinearRegression()
-clf.fit(X, y)
+    # np.random.seed(42)
+    # X = np.array(sorted(list(range(5))*20)) + np.random.normal(size=100, scale=0.5)
+    # y = np.array(sorted(list(range(5))*20)) + np.random.normal(size=100, scale=0.25)
 
-plt.style.use('fivethirtyeight')
+    print(X)
+    print(y)
 
-plt.scatter(X, y, color='black')
-plt.plot(X, clf.predict(X))
-plt.gca().set_title("Gradient Descent Linear Regressor")
-#windows
-plt.savefig("matplotlib.png")
+    clf = GradientDescentLinearRegression()
+    clf.fit(X, y)
+
+    plt.style.use('fivethirtyeight')
+
+    plt.scatter(X, y, color='black')
+    plt.plot(X, clf.predict(X))
+    plt.gca().set_title("Gradient Descent Linear Regressor")
+    # windows
+    # plt.savefig("matplotlib.png")
+
+    # mac
+    plt.show()
