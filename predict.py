cd21191384036e6bf508978730f5a96283a4576d
fel-hita <farouk.elhitary@gmail.com>
2020-11-26 17:49:42 +0100

    predict thetas initialized to 0 if training thetas are not present

--- predict.py
+++ predict.py
@@ -1,11 +1,18 @@
 import numpy as np
 import sys
+import os.path
+from os import path
+import array as arr
 
 def estimate_price(theta_0, theta_1, x):
 	return theta_0 + theta_1 * x
 
 def main():
-	theta = np.loadtxt("theta.csv", dtype = np.longdouble, delimiter = ',')
+	
+	if(path.exists('theta.csv')):
+		theta = np.loadtxt("theta.csv", dtype = np.longdouble, delimiter = ',')
+	else:
+		theta = arr.array('d',[0,0])
 	while 1:
 		print('Provide the mileage: ')
 		try:
@@ -17,7 +24,6 @@ def main():
 				print('Input should be a positive integer.')
 		except:
 			sys.exit('Error input.')
-
 	print(estimate_price(theta[0], theta[1], mileage))
 
 if __name__ == "__main__":
